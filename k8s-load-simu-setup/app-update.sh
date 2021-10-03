4b97329825d8385418efed4e75b2a307886a4c84
Biran <birayaha@f8ffc21f4dbb.ant.amazon.com>
2021-10-02 18:18:16 -0700

    load db

--- k8s-load-simu-setup/app-update.sh
+++ k8s-load-simu-setup/app-update.sh
@@ -4,5 +4,7 @@ while true
 do
 # /app-update-from-func.sh &
  /app-update-from-sqs.sh &
+ /app-update-from-sqs.sh &
+ /app-update-from-sqs.sh &
  sleep `awk -v min=5 -v max=10 'BEGIN{srand(); print int(min+rand()*(max-min+1))}'`
 done
