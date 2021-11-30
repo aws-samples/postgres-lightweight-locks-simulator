128e6ec7d173a978597da84f19e0c08a82511585
Biran <birayaha@3c063041fedb.ant.amazon.com>
2021-11-30 13:50:25 -0800

    b_3255

--- k8s-load-simu-setup/app-load.sh
+++ k8s-load-simu-setup/app-load.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+export PGPASSWORD=$DB_PASSWORD
+export PGHOST=$DBHOST
+export PGUSER=$DB_USER
+
 while true
 do
-    i=`awk -v min=1 -v max=10 'BEGIN{srand(); print int(min+rand()*(max-min+1))}'`
-    #psql -A -q -t -w postgres://postgres$i:'Admin1234!'@af18acdd595674d10a50a60125624e72-8c1396ad4d584287.elb.us-west-2.amazonaws.com:5432/postgres -c "begin;SELECT * FROM orders WHERE  int_notnull_1<3 and created_at > NOW() - '2 hour'::INTERVAL and updated_at < NOW() - '1.99999 hour'::INTERVAL;commit;"
-    psql -A -q -t -w postgres://postgres$i:'Admin1234!'@af18acdd595674d10a50a60125624e72-8c1396ad4d584287.elb.us-west-2.amazonaws.com:5432/postgres -f insert-into-order-many.sql
-    sleep `awk -v min=5 -v max=7 'BEGIN{srand(); print int(min+rand()*(max-min+1))}'`
+  sleep `awk -v min=10 -v max=30 'BEGIN{srand(); print int(min+rand()*(max-min+1))}'`
+  id=`psql -A -q -t -w -c "begin;select count(*),int_notnull_2/1000,int_notnull_3/1000 from orders WHERE  created_at > NOW() - '10 minute'::INTERVAL and created_at < NOW() - '7 minute'::INTERVAL  group by int_notnull_2/1000,int_notnull_3/1000;"`
+  echo "psql exit code="$?
+  if [ -z "$id" ]
+  then
+    echo "ERR-DB"
+  fi
+  sleep `awk -v min=10 -v max=30 'BEGIN{srand(); print int(min+rand()*(max-min+1))}'`
 done
