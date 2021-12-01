bc8bd6cc75e942f564ac1df32eba3615f60f1588
root <root@ip-192-168-74-110.us-west-2.compute.internal>
2021-12-01 00:09:40 +0000

    b_7735

--- k8s-load-simu-setup/app-load.sh
+++ k8s-load-simu-setup/app-load.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 export PGPASSWORD=$DB_PASSWORD
 export PGHOST=$DBHOST
@@ -7,11 +7,15 @@ export PGUSER=$DB_USER
 while true
 do
   sleep `awk -v min=10 -v max=30 'BEGIN{srand(); print int(min+rand()*(max-min+1))}'`
-  id=`psql -A -q -t -w -c "begin;select count(*),int_notnull_2/1000,int_notnull_3/1000 from orders WHERE  created_at > NOW() - '10 minute'::INTERVAL and created_at < NOW() - '7 minute'::INTERVAL  group by int_notnull_2/1000,int_notnull_3/1000;"`
+  id=`psql -A -q -t -w -c "begin;select /*from docker minute range*/ count(*),int_notnull_2/1000,int_notnull_3/1000 from orders WHERE  created_at > NOW() - '10 minute'::INTERVAL and created_at < NOW() - '7 minute'::INTERVAL  group by int_notnull_2/1000,int_notnull_3/1000;"`
   echo "psql exit code="$?
   if [ -z "$id" ]
   then
     echo "ERR-DB"
   fi
+  sleep `awk -v min=10 -v max=30 'BEGIN{srand(); print int(min+rand()*(max-min+1))}'`
+  id=`psql -A -q -t -w -c "begin;select /*from docker day range*/ count(*) count,int_notnull_2/1000,int_notnull_3/1000 from orders WHERE  created_at > NOW() - '2 day'::INTERVAL and created_at < NOW() - '1 day'::INTERVAL  group by int_notnull_2/1000,int_notnull_3/1000 order by count desc limit 20;"`
+  sleep `awk -v min=10 -v max=30 'BEGIN{srand(); print int(min+rand()*(max-min+1))}'`
+  id=`psql -A -q -t -w -c "begin;select /*from docker day range*/ count(*) count,int_notnull_2/1000,int_notnull_3/1000 from orders WHERE  created_at > NOW() - '3 day'::INTERVAL and created_at < NOW() - '2 day'::INTERVAL  group by int_notnull_2/1000,int_notnull_3/1000 order by count desc limit 20;"`
   sleep `awk -v min=10 -v max=30 'BEGIN{srand(); print int(min+rand()*(max-min+1))}'`
 done
