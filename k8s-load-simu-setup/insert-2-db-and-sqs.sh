09a5f56ab14f0b7c1e5f25481fc5d968be83c959
Biran <birayaha@3c063041fedb.ant.amazon.com>
2022-02-08 16:16:20 -0800

    b_15508

--- k8s-load-simu-setup/insert-2-db-and-sqs.sh
+++ k8s-load-simu-setup/insert-2-db-and-sqs.sh
@@ -5,13 +5,13 @@ export PGHOST=$DBHOST
 
 while true
 do
-  id=`psql -A -q -t -U postgres -w -f insert-into-order.sql`
+  ret_val=`psql -A -q -t -U postgres -w -f insert-into-order.sql`
   echo "psql exit code="$?
-  if [ -z "$id" ]
+  if [ -z "$ret_val" ]
   then
     echo "ERR-DB"
   else
-    aws sqs send-message --queue-url ${QUEUE_URL} --message-body "${id}" 
+    aws sqs send-message --queue-url ${QUEUE_URL} --message-body "${ret_val}" 
     echo "sqs exit code="$?
   fi
   #sleep `awk -v min=10 -v max=30 'BEGIN{srand(); print int(min+rand()*(max-min+1))}'`
